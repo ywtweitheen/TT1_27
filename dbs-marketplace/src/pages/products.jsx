6c3ac34a6e1e40453a772a7f1d7e0f044460f12b
ywtweitheen <ywtweitheen@hotmail.com>
2021-09-30 14:29:35 +0800

    Update product fetch api url

--- dbs-marketplace/src/pages/products.jsx
+++ dbs-marketplace/src/pages/products.jsx
@@ -26,7 +26,7 @@ class Products extends Component {
 
 	componentDidMount() {
 		// fetch('../products.json')
-		fetch('http://localhost:5000/products')
+		fetch('http://localhost:8000/products')
 			// convert res to json
 			.then((res) => res.json())
 			.then((json) => {
